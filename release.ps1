efc93f48a7776c4da0f7e42ffa8fb6136f530b10
Fetiss <i@timel.ink>
2023-02-15 19:51:06 +0800

    chore: 构建

--- release.ps1
+++ release.ps1
@@ -1,1 +1,1 @@
-flet pack ".\interface.py" --add-data "unity;UnityPy/resources" --add-data "resources;resources" --add-data "input;input" --name "No.86 卡片翻译切换" --icon "images/icon.png"
+flet pack ".\interface.py" --add-data "unity;UnityPy/resources" --add-data "resources;resources" --add-data "input;input" --name "MD 汉化版本切换" --icon "images/icon.png"
