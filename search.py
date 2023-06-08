772605c9c7550ab233bb93049d18f5cc4ec979d1
mikualpha <mikualpha@hotmail.com>
2023-06-08 20:22:44 +0800

    [fix] 字体安装流程修改

---
+++ search.py
@@ -0,0 +1,38 @@
+import os
+import UnityPy
+
+
+def search_card_obj_list(path_game_root: str) -> dict[str, str]:
+    card_obj_name = (
+        'CARD_Desc',
+        'CARD_Indx',
+        'CARD_Name',
+        'Card_Part',
+        'Card_Pidx',
+    )
+
+    card_obj_list = {}
+    for root, dirs, files in os.walk(os.path.join(path_game_root, 'LocalData')):
+        for file in files:
+            file_path = os.path.join(root, file)
+            env = UnityPy.load(file_path)
+            for obj in env.objects:
+                if obj.type.name == "TextAsset":
+                    data = obj.read()
+                    name = data.name
+                    if name in card_obj_name:
+                        if 'zh-cn' not in data.container:  # 不是中文的跳过
+                            continue
+
+                        print(name, data.container, file_path)
+                        if name not in card_obj_list:
+                            card_obj_list[name] = file
+
+            if len(card_obj_list) >= len(card_obj_name):
+                break
+
+        if len(card_obj_list) >= len(card_obj_name):
+            break
+
+    print(repr(card_obj_list))
+    return card_obj_list
