8d54e251b2c804b2d568c0a69ed5b3afcb3e31a7
mikualpha <24711249+mikualpha@users.noreply.github.com>
2023-09-08 00:52:14 +0800

    [update] 更新发布环境版本

--- release.ps1
+++ release.ps1
@@ -1,1 +1,3 @@
-flet pack ".\interface.py" --add-data "unity;UnityPy/resources" --add-data "resources;resources" --add-data "input;input" --name "MDTR_" --icon "images/icon.png"
+$version = Read-Host "Please enter version"
+
+flet pack ".\interface.py" --add-data "unity;UnityPy/resources" --add-data "resources;resources" --add-data "input;input" --name "MDTR_v$version" --icon "images/icon.png" --file-version $version
