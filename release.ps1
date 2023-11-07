2b47d7a91772b3595eccd4da9b7a1a05d5be03b5
MikuAlpha <24711249+mikualpha@users.noreply.github.com>
2023-11-08 00:49:26 +0800

    [delete] 移除旧文件

--- release.ps1
+++ release.ps1
@@ -1,3 +1,3 @@
 $version = Read-Host "Please enter version"
 
-flet pack ".\interface.py" --add-data "unity;UnityPy/resources" --add-data "resources;resources" --add-data "input;input" --name "MDTR_v$version" --icon "images/icon.png" --file-version $version
+flet pack ".\interface.py" --add-data "unity;UnityPy/resources" --add-data "resources;resources" --name "MDTR_v$version" --icon "images/icon.png" --file-version $version
