f78ea26690d91ad78e334da0dfb3efd16840d8e0
MikuAlpha <24711249+mikualpha@users.noreply.github.com>
2024-06-15 15:01:07 +0800

    [refactor] 迁移unify函数

--- api.py
+++ api.py
@@ -1,12 +1,84 @@
 import json
 from os import path
 from typing import Callable, Literal, NoReturn, TypedDict, Union
-
+from utils import get_resource_path, q2b_string
+from functools import reduce
 import requests
 
-from utils import get_resource_path, q2b_string
 
 NameDesc = TypedDict("Card", {"name": str, "desc": str})
+
+
+class UnifyChar:
+    @staticmethod
+    def unify_pendulum_desc(desc: str) -> str:
+        """
+        统一各种灵摆的格式
+
+        样例：①...\n【灵摆效果】\n①...
+        """
+        if not desc.startswith("←"):  # 这种判断方式挺粗糙的
+            return desc  # 不是灵摆就算了
+        separator = "【灵摆效果】"
+        ch_separator = "【怪兽效果】"
+        if desc.find(ch_separator) == -1:
+            ch_separator = "【怪兽描述】"
+        if desc.find(ch_separator) == -1:
+            ch_separator = "【Monster Effect】"  # 怎么还有英文的啊 这是人能干出来的事吗
+        if desc.find(ch_separator) == -1:
+            ch_separator = "【Flavor Text】"  # 居然还有这种无比阴间的...
+
+        if desc.find(ch_separator) == -1:
+            return desc  # 还是找不到就算了
+
+        p_start = desc.find("→")
+        cn_separator_start = desc.find(ch_separator)
+        p_effect = desc[p_start + 1: cn_separator_start - 1].strip()
+        monster_effect = desc[cn_separator_start + len(ch_separator):].strip()
+
+        res = ""
+        res += monster_effect
+        if p_effect != "":
+            res = f"{res}\n{separator}\n{p_effect}"
+
+        return res
+
+    @staticmethod
+    def unify_separator(desc: str) -> str:
+        """
+        按照原版处理分隔符
+        1. ①...\n【灵摆效果】\n①...
+        2. 召唤限制\n①
+        3. ① ②这些的前面不会有\r\n
+        """
+        orders_without_first = ["②", "③", "④", "⑤", "⑥", "⑦"]
+        ch_separator = "【灵摆效果】"
+        for order in ["②", "③", "④", "⑤", "⑥", "⑦"]:
+            for prefix in ["\r\n", "\n", "\r"]:
+                desc = desc.replace(f"{prefix}{order}", order)
+        # 处理①
+        for prefix in ["\r\n", "\n", "\r"]:
+            desc = desc.replace(f"。{prefix}①", "。①")
+        desc = desc.replace("\r\n", "\n").strip()
+        # 在●后面紧跟的一个②或者③，它们的前面有\n
+        parts: list[str] = [f"{'●' if i else ''}{x}" for i, x in enumerate(desc.split("●"))]
+        for part in parts:
+            if not part.startswith('●'):
+                continue
+            index = len(part) - 1
+            for order in orders_without_first:
+                if (i := part.find(order)) != -1:
+                    index = min(index, i)
+            if (i := part.find("【灵摆效果】")) != -1:
+                index = min(index, i)
+            if index != len(part) - 1 and part[index] != ch_separator[0]:
+                parts[parts.index(part)] = f"{part[:index]}\n{part[index:]}"
+        # merge parts
+        return reduce(lambda x, y: f"{x}{y}", parts)
+
+    @staticmethod
+    def unity(desc: str) -> str:
+        return UnifyChar.unify_separator(UnifyChar.unify_pendulum_desc(desc))
 
 
 class CardsCache(TypedDict):
@@ -39,7 +111,7 @@ class CacheManager(object):
         }
 
         cls.cardCache[str(cid)] = cache_obj
-        print('Add Cache:', cid, md_name, cn_name, len(cls.cardCache))
+        print('Add Cache:', cid, md_name, '->', cn_name, len(cls.cardCache))
         cls.save_cache()
 
     @classmethod
@@ -134,6 +206,7 @@ def api(
         if (p_desc := item["text"]["pdesc"]) != "":
             desc = f"{desc}\n【灵摆效果】\n{p_desc}"
 
+        desc = UnifyChar.unity(desc)  # 修正灵摆...修正\r\n
         if dev_mode:
             CacheManager.add_cache(cid,
                                    jp_name=item.get('jp_name', ''),
@@ -154,3 +227,22 @@ def api(
         except:
             network_error_cb()
             return None
+
+
+if __name__ == "__main__":
+    # 测试
+    print(
+        [
+            UnityChar.unify_separator(
+                UnityChar.unify_pendulum_desc(
+                    """
+        ①：1回合1次，可以从以下效果选择1个发动。\r\n●以自己场上1只「X-首领加农」为对象，把这张卡当作装备卡使用给那只怪兽装备。装备怪兽被战斗・效果破坏的场合，作为代替把这张卡破坏。\r\n●装备的这张卡特殊召唤。\r\n【灵摆效果】\r\n①：装备怪兽的攻击力・守备力上升400。
+        """
+                )
+            )
+        ]
+    )
+    print([UnityChar.unify_separator(
+        "①：1回合1次，可以从以下效果选择1个发动。\r\n●以自己场上1只「X-首领加农」为对象，把这张卡当作装备卡使用给那只怪兽装备。装备怪兽被战斗・效果破坏的场合，作为代替把这张卡破坏。\r\n●装备的这张卡特殊召唤。\r\n【灵摆效果】\r\n②：装备怪兽的攻击力・守备力上升400。")])
+    print([UnityChar.unify_separator(
+        "①：1回合1次，可以从以下效果选择1个发动。\r\n●以自己场上1只「X-首领加农」为对象，把这张卡当作装备卡使用给那只怪兽装备。装备怪兽被战斗・效果破坏的场合，作为代替把这张卡破坏。\r\n●装备的这张卡特殊召唤。\r\n②：装备怪兽的攻击力・守备力上升400。●装备的这张卡特殊召唤。\r\n\r\n③：装备怪兽的攻击力・守备力上升400。")])
--- translate.py
+++ translate.py
@@ -1,4 +1,3 @@
-from functools import reduce
 from typing import Callable
 
 from tqdm import tqdm
@@ -8,93 +7,6 @@ from api import api, api_local, CacheManager
 from hint import CardRawData, CardRawDataItem
 
 
-def unify_pendulum_desc(desc: str) -> str:
-    """
-    统一各种灵摆的格式
-
-    样例：①...\n【灵摆效果】\n①...
-    """
-    if not desc.startswith("←"):  # 这种判断方式挺粗糙的
-        return desc  # 不是灵摆就算了
-    separator = "【灵摆效果】"
-    ch_separator = "【怪兽效果】"
-    if desc.find(ch_separator) == -1:
-        ch_separator = "【怪兽描述】"
-    if desc.find(ch_separator) == -1:
-        ch_separator = "【Monster Effect】"  # 怎么还有英文的啊 这是人能干出来的事吗
-    if desc.find(ch_separator) == -1:
-        ch_separator = "【Flavor Text】"  # 居然还有这种无比阴间的...
-
-    if desc.find(ch_separator) == -1:
-        return desc  # 还是找不到就算了
-
-    p_start = desc.find("→")
-    cn_separator_start = desc.find(ch_separator)
-    p_effect = desc[p_start + 1 : cn_separator_start - 1].strip()
-    monster_effect = desc[cn_separator_start + len(ch_separator) :].strip()
-
-    res = ""
-    res += monster_effect
-    if p_effect != "":
-        res = f"{res}\n{separator}\n{p_effect}"
-
-    return res
-
-
-def unify_separator(desc: str) -> str:
-    """
-    按照原版处理分隔符
-    1. ①...\n【灵摆效果】\n①...
-    2. 召唤限制\n①
-    3. ① ②这些的前面不会有\r\n
-    """
-    orders_without_first = ["②", "③", "④", "⑤", "⑥", "⑦"]
-    ch_separator = "【灵摆效果】"
-    for order in ["②", "③", "④", "⑤", "⑥", "⑦"]:
-        for prefix in ["\r\n", "\n", "\r"]:
-            desc = desc.replace(f"{prefix}{order}", order)
-    # 处理①
-    for prefix in ["\r\n", "\n", "\r"]:
-        desc = desc.replace(f"。{prefix}①", "。①")
-    desc = desc.replace("\r\n", "\n").strip()
-    # 在●后面紧跟的一个②或者③，它们的前面有\n
-    parts: list[str] = [f"{'●' if i else ''}{x}" for i, x in enumerate(desc.split("●"))]
-    for part in parts:
-        if not part.startswith('●'):
-            continue
-        index = len(part) - 1
-        for order in orders_without_first:
-            if (i := part.find(order)) != -1:
-                index = min(index, i)
-        if (i := part.find("【灵摆效果】")) != -1:
-            index = min(index, i)
-        if index != len(part) - 1 and part[index] != ch_separator[0]:
-            parts[parts.index(part)] = f"{part[:index]}\n{part[index:]}"
-    # merge parts
-    return reduce(lambda x, y: f"{x}{y}", parts)
-
-
-def unity(desc: str) -> str:
-    return unify_separator(unify_pendulum_desc(desc))
-
-
-if __name__ == "__main__":
-    # 测试
-    print(
-        [
-            unify_separator(
-                unify_pendulum_desc(
-                    """
-        ①：1回合1次，可以从以下效果选择1个发动。\r\n●以自己场上1只「X-首领加农」为对象，把这张卡当作装备卡使用给那只怪兽装备。装备怪兽被战斗・效果破坏的场合，作为代替把这张卡破坏。\r\n●装备的这张卡特殊召唤。\r\n【灵摆效果】\r\n①：装备怪兽的攻击力・守备力上升400。
-        """
-                )
-            )
-        ]
-    )
-    print([unify_separator("①：1回合1次，可以从以下效果选择1个发动。\r\n●以自己场上1只「X-首领加农」为对象，把这张卡当作装备卡使用给那只怪兽装备。装备怪兽被战斗・效果破坏的场合，作为代替把这张卡破坏。\r\n●装备的这张卡特殊召唤。\r\n【灵摆效果】\r\n②：装备怪兽的攻击力・守备力上升400。")])
-    print([unify_separator("①：1回合1次，可以从以下效果选择1个发动。\r\n●以自己场上1只「X-首领加农」为对象，把这张卡当作装备卡使用给那只怪兽装备。装备怪兽被战斗・效果破坏的场合，作为代替把这张卡破坏。\r\n●装备的这张卡特殊召唤。\r\n②：装备怪兽的攻击力・守备力上升400。●装备的这张卡特殊召唤。\r\n\r\n③：装备怪兽的攻击力・守备力上升400。")])
-
-
 def card_translate(
     archived_Data: CardRawData,
     card_raw_data: CardRawData,
@@ -102,6 +14,8 @@ def card_translate(
     network_error_cb: Callable[[], None] = lambda: None,
     dev_mode: bool = False,
 ) -> CardRawData:
+
+    FORCE_UPDATE_CARD_CACHE = False  # 强制更新卡片缓存
 
     # 统计同名卡的函数
     # card_name = {}
@@ -163,29 +77,34 @@ def card_translate(
         desc_md = item["desc"]["zh-cn"]
 
         progress_update_cb(f"{i}/{len(card_raw_data)}")
+
+        if FORCE_UPDATE_CARD_CACHE:
+            CacheManager.load_cache()
+
         # 新翻译文件(使用CID查找)
-        if result_api_local := api_local(cid):
+        if (result_api_local := api_local(cid)) and not FORCE_UPDATE_CARD_CACHE:
             # 在本地缓存中找到对应的日文名
             item["name"]["custom"] = result_api_local["name"]
-            item["desc"]["custom"] = unity(result_api_local["desc"])
+            item["desc"]["custom"] = result_api_local["desc"]
             continue
 
+        # 翻译API
         if result_api := api(name_md, cid, desc_md, network_error_cb, dev_mode):
             # print('API Remote Found:', name_md, result_api)
 
             # 在API中找到了对应的日文名
             item["name"]["custom"] = result_api["name"]
-            item["desc"]["custom"] = unity(result_api["desc"])  # 修正灵摆...修正\r\n
+            item["desc"]["custom"] = result_api["desc"]
             continue
 
         # 旧版翻译文件
-        if result_archived := search_archived_data(name_md, desc_md):
-            # print('Local JSON Found:', name_md)
-            
-            # 在已归档数据中找到了对应的日文名
-            item["name"]["custom"] = result_archived["name"]["custom"]
-            item["desc"]["custom"] = unity(result_archived["desc"]["custom"])
-            continue
+        # if result_archived := search_archived_data(name_md, desc_md):
+        #     # print('Local JSON Found:', name_md)
+        #
+        #     # 在已归档数据中找到了对应的日文名
+        #     item["name"]["custom"] = result_archived["name"]["custom"]
+        #     item["desc"]["custom"] = unity(result_archived["desc"]["custom"])
+        #     continue
 
         # 未找到对应的日文名
         if dev_mode:
